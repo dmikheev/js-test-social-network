da1e24985150574d814746e8d2dbb787f6a19d2e
Dmitry Mikheev <dmikheev@users.noreply.github.com>
2018-06-12 22:01:20 +0300

    Update webpack to v4

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,50 +1,73 @@
 var webpack = require('webpack');
 
-module.exports = {
-  entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    './src/index.jsx'
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'react-hot!babel'
-      },
-      {
-        test: /\.css$/,
-        exclude: /global\.css$/,
-        loader: 'style-loader!css-loader?modules&localIdentName=[name]_[local]_[hash:base64]!postcss-loader'
-      },
-      {
-        test: /global\.css$/,
-        loader: 'style-loader!css-loader!postcss-loader'
+module.exports = (env) => {
+  const isProd = env.production === true;
+
+  const entries = [];
+  !isProd && entries.push('webpack-dev-server/client?http://localhost:8080');
+  !isProd && entries.push('webpack/hot/only-dev-server');
+  entries.push('./src/index.jsx');
+
+  const jsLoaders = [];
+  !isProd && jsLoaders.push('react-hot-loader');
+  jsLoaders.push('babel-loader');
+
+  const publicPath = isProd ? 'http://localhost/' : 'http://localhost:8080/';
+
+  const plugins = [];
+  !isProd && plugins.push(new webpack.HotModuleReplacementPlugin());
+
+  return {
+    entry: entries,
+    mode: isProd ? 'production' : 'development',
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: jsLoaders,
+        },
+        {
+          test: /\.css$/,
+          exclude: /global\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                localIdentName: '[name]_[local]_[hash:base64]',
+                modules: true
+              }
+            },
+            'postcss-loader'
+          ]
+        },
+        {
+          test: /global\.css$/,
+          loader: 'style-loader!css-loader!postcss-loader'
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    output: {
+      path: __dirname + '/dist',
+      publicPath,
+      filename: 'bundle.js'
+    },
+    devServer: {
+      contentBase: './dist',
+      hot: true,
+      proxy: {
+        '/api/**': {
+          target: 'http://localhost',
+          secure: false,
+          logLevel: 'debug'
+        }
       }
-    ]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  output: {
-    path: __dirname + '/dist',
-    publicPath: 'http://localhost:8080/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-    proxy: {
-      '/api/**': {
-        target: 'http://localhost',
-        secure: false,
-        logLevel: 'debug'
-      }
-    }
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.OldWatchingPlugin()
-  ]
+    },
+    plugins,
+  };
 };
