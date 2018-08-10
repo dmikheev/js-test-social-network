db9e4114efa295ea17eb1598147c132047a3147c
Dmitry Mikheev <dmikheev@users.noreply.github.com>
2018-08-10 19:09:54 +0300

    Remove old code

--- src/handlers/presenters/friendshipPresenter.ts
+++ src/handlers/presenters/friendshipPresenter.ts
@@ -1,21 +1,10 @@
 import { IFriendshipDocument } from '../../models/friendship';
-import UserPresenter, { IUserClientData } from './userPresenter';
 
 interface IFriendshipClientData {
   accepted: boolean;
   id: string;
   receiverId: string;
   senderId: string;
-}
-
-/** @deprecated */
-interface IFriendshipClientDataOld {
-  [key: string]: any;
-
-  accepted: boolean;
-  id: string;
-  receiver: IUserClientData;
-  sender: IUserClientData;
 }
 
 const FriendshipPresenter = {
@@ -25,16 +14,6 @@ const FriendshipPresenter = {
       id: friendship._id,
       receiverId: friendship.receiverId.toString(),
       senderId: friendship.senderId.toString(),
-    };
-  },
-
-  /** @deprecated */
-  getDataOld(friendship: any): IFriendshipClientDataOld {
-    return {
-      accepted: friendship.accepted,
-      id: friendship._id,
-      receiver: UserPresenter.getData(friendship.receiverId),
-      sender: UserPresenter.getData(friendship.senderId),
     };
   },
 };
